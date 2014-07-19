feae602161060d6b36f2961e7563d15596208326
Tomas Trajan <tomas.trajan@gmail.com>
2014-07-19 23:02:56 +0200

    Initial commit

---
+++ config/adapters.js
@@ -0,0 +1,29 @@
+/**
+ * Global adapter config
+ * 
+ * The `adapters` configuration object lets you create different global "saved settings"
+ * that you can mix and match in your models.  The `default` option indicates which 
+ * "saved setting" should be used if a model doesn't have an adapter specified.
+ *
+ * Keep in mind that options you define directly in your model definitions
+ * will override these settings.
+ *
+ * For more information on adapter configuration, check out:
+ * http://sailsjs.org/#documentation
+ */
+
+module.exports.adapters = {
+
+    'default': 'postgres',
+
+    postgres: {
+        module   : 'sails-postgresql',
+        host     : process.env.POSTGRES_HOST,
+        port     : process.env.POSTGRES_PORT,
+        user     : process.env.POSTGRES_USER,
+        password : process.env.POSTGRES_PASSWORD,
+        database : process.env.POSTGRES_DATABASE,
+
+        schema   : true //This makes sure that sails matches the database schema to your models.
+    }
+};
