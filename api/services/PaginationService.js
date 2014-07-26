9fd4362451ee752ccbfe908f7ba78d6cd50cf2f4
Tomas Trajan <tomas.trajan@gmail.com>
2014-07-26 13:28:02 +0200

    Fixed pagination url handling, adjusted index layout and styles

--- api/services/PaginationService.js
+++ api/services/PaginationService.js
@@ -2,6 +2,7 @@ var PaginationService = {
 
     getPagination: function(req) {
         return  {
+            url: req.protocol + '://' + req.get('Host') + req.path,
             page: parseInt(req.param('page') || 0),
             pageSize: parseInt(req.param('pageSize') || 10)
         }
